c9476b4306651c25cc6e0b6248833ca13a661b37
Arialita <alejandro_aguilar_1996@hotmail.com>
2023-01-04 15:42:43 -0300

    Primer commit

---
+++ src/main/java/com/myportfolio/BackendPortfolio/BackendPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.myportfolio.BackendPortfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myportfolio/BackendPortfolio/BackendPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myportfolio.BackendPortfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
