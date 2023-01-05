e64732e5c025fd98ba8f6cb0e9bd08c8dc62b5e0
Arialita <alejandro_aguilar_1996@hotmail.com>
2023-01-04 22:16:13 -0300

    Empiezo con persona

---
+++ src/main/java/com/myportfolio/BackendPortfolio/model/Educacion.java
@@ -0,0 +1,11 @@
+package com.myportfolio.BackendPortfolio.model;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@Entity
+public class Educacion {
+    
+}
